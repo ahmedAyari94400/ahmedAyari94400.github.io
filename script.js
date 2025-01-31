2ee7a5caaadd03dd350800646063f9b543568055
VitryAhmed <aayari97@gmail.com>
2025-01-31 09:46:35 +0100

    MaJfichiers

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-let visible = document.getElementById("visibleHidden");
-// On ajoute un évènement pour écouter le clic sur le paragrapke
-visible.addEventListener("click");
-if (getComputedStyle("pres").display ==none)
+// On cré une variable pour voir ce qui se passe sur le lien a href
+
+// On ajoute un évènement pour écouter le clic sur le lien
+
+function displayMessage()
 {
-    getComputedStyle("pres").display=true;
+    console.log("into function", document.getElementById("pres"));
+    // Si le lien est cliqué et que la div est invisble on la rend visible sinon on la rend invisible
+    // if (document.getElementById("pres").display===none)
+    //     {
+    //         document.getElementById("pres").innerHTML
+    //         console.log("display");
+    //     }
+    //     else
+    //     {
+    //         //document.getElementById("pres").display=none;
+    //         console.log("not display");
+    //     }
+    document.getElementById("pres").displayMessage;
 }
-else
-{
-    getComputedStyle("pres").display=false;
-}
+document.addEventListener("click", displayMessage);
+
+/*document.addEventListener("click", function(){
+  document.getElementById("demo").innerHTML = "Hello World!";
+});*/ 
+
+//display function:
+
